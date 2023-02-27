a87a96a838949a3caec0d7142c1be5e2b117e634
Eudis Alves <deleudis@gmail.com>
2023-02-26 22:53:25 -0300

    adicionando icones de tecnologias

--- src/components/TecContainer.tsx
+++ src/components/TecContainer.tsx
@@ -1,35 +1,42 @@
-import { Key, ReactNode } from 'react'
+
 import {
     DiHtml5,
     DiCss3,
     DiNodejsSmall,
-    DiTypo3,
-    DiReact
+    DiReact,
+    DiSass,
+    DiJsBadge,
+    
 } from 'react-icons/di'
+import {
+    SiTypescript,
+    SiVite
+} from "react-icons/si"
 
 import '../styles/components/teccontainer.sass'
 
 const tech = [
-    {id: "html", name: "HTML5", icon: <DiHtml5 />},
-    {id: "css", name: "Css3", icon: <DiCss3 />},
-    {id: "node", name: "node.js", icon: <DiNodejsSmall />},
-    {id: "typo", name: "Typescript", icon: <DiTypo3 />},
-    {id: "react", name: "React", icon: <DiReact />}
-]
+    { id: "html", name: "HTML5", icon: <DiHtml5 />},
+    { id: "css", name: "Css3", icon: <DiCss3 />},
+    { id: "js", name: "JavaScript", icon: <DiJsBadge />},
+    { id: "node", name: "node.js", icon: <DiNodejsSmall />},
+    { id: "type", name: "TypeScrpit", icon: <SiTypescript />},
+    { id: "vite", name: "Vite", icon: <SiVite />},
+    { id: "react", name: "React", icon: <DiReact />},
+    { id: "sass", name: "Sass", icon: <DiSass />},
+];
 
 export default function tecContainer() {
     return (
-        <section id='tec-container'>
+        <section id='tech-container'>
             <h2>Tecnologias</h2>
             <div className="tech-grid">
-                {tech.map((tech: {
-                    [x: string]: ReactNode; id: Key | null | undefined; 
-                     }) => (
-                    <div className="tec-card" id='tech.id' key={tech.id}>
+                {tech.map((tech)=> (
+                    <div className="tech-card" id={tech.id} key={tech.id}>
                         {tech.icon}
                         <div className="tech-info">
                             <h3>{tech.name}</h3>
-                            <p>informações</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing eli.</p>
                         </div>
                     </div>
                 ))}
